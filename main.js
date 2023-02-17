3c981e69c23d9065b9f62cec2a2c9bbf29885a25
unknown <mr_baxa>
2023-02-17 18:47:21 +0500

    Data types

---
+++ main.js
@@ -0,0 +1,40 @@
+// let name = 17;
+
+// let bool = true
+
+// alert()
+
+// function name(){}
+
+// name()
+// let name = "Bohodir ";
+// let lastName = "Rahmonov";
+// let fullName = `${name} ${lastName}`;
+
+// let fullName = name + lastName;
+
+// let inputValue = prompt("Sizning yoshingiz", "22");
+// console.log(+inputValue);
+
+// let name = "Bohodir";
+// let result = name - "b";
+
+// let a = 5;
+// let b = 3;
+
+// prompt("Son Kiriting");
+
+// let result = a % 3;
+
+let a = 5;
+let b = 5;
+
+if (a == b) {
+  console.log("Ha teng");
+} else {
+  console.log("Teng emas");
+}
+
+// let count;
+
+// console.log(count + 3);
