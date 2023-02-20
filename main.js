244a7499060b9132420c222e7a73d92aa6b91f82
unknown <mr_baxa>
2023-02-20 18:50:59 +0500

    Logical opertaors

--- main.js
+++ main.js
@@ -1,40 +1,86 @@
-// let name = 17;
+// const name = {};
 
-// let bool = true
+// console.log(typeof name);
 
-// alert()
+// let a = "5";
+// let b = "4";
 
-// function name(){}
+// let result = a + b;
 
-// name()
-// let name = "Bohodir ";
-// let lastName = "Rahmonov";
-// let fullName = `${name} ${lastName}`;
+// let num = 2;
+// // num += 5; // -> num = num +5
 
-// let fullName = name + lastName;
+// // num **= 5; num = num  ** 5
 
-// let inputValue = prompt("Sizning yoshingiz", "22");
-// console.log(+inputValue);
+// num++; // post prefiks
+// ++num; // prefix
+// console.log(num);
 
-// let name = "Bohodir";
-// let result = name - "b";
+// let num = 1;
 
-// let a = 5;
-// let b = 3;
+// alert(num++) // eski qiymatni qaytarib beradi.
 
-// prompt("Son Kiriting");
+// alert(++num); // yangi qiymatni qaytarib beradi.
 
-// let result = a % 3;
+// let a = 1,
+//   b = 1;
 
-let a = 5;
-let b = 5;
+// let c = ++a;
+// let d = b++;
 
-if (a == b) {
-  console.log("Ha teng");
-} else {
-  console.log("Teng emas");
-}
+// console.log("c -> ", c, "d ->", d);
 
-// let count;
+// let bool = "Bohodir" === "bohodir";
 
-// console.log(count + 3);
+// console.log(bool);
+
+// Falsy qiymatlar: 0,false,undefined,null,"",NaN
+
+// console.log("bohodir".length);
+//
+// if (false) {
+//   console.log("True qiymati");
+// } else if (true) {
+//   console.log("Yana 2 true degan qiymat");
+// } else {
+//   console.log("False qiymat boladi");
+// }
+
+// let age = prompt("Sizning yoshingiz nechida");
+
+// let result =
+//   age < 3
+//     ? "Hi baby"
+//     : age < 18
+//     ? "Hello!"
+//     : age < 100
+//     ? "Greetings"
+//     : "Ovoz bermasin";
+
+// let result = age === 18;
+
+// console.log(result);
+
+// if (true || false) {
+
+// } else {
+// }
+
+// let result = 0 || 3 || 7;
+
+// if (0 || 3) {
+//   console.log("Truethy qiymat");
+// } else {
+//   console.log("Falsy qiymant");
+// }
+
+// let result = 0 && 0 && 6;
+
+// if (result) {
+//   console.log("Truety qiymat");
+// } else {
+//   console.log("Falsy qiymat");
+// }
+
+// console.log(!!undefined);
+// alert(alert(1) || 2 || alert(3));
