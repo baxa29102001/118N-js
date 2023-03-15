84408b902cb153b7179587fa9d44638db4c2d748
mr_baxa <bohodir29102001@gmail.com>
2023-03-15 18:54:59 +0500

    fix

--- main.js
+++ main.js
@@ -1,224 +1,82 @@
-// const arr = [0, 1, "String", true, false, 999989989898989, {}, []];
-// const arr2 = arr;
-// Elemetni oxiridan qoshib beradi
+// window.SpeechRecognition =
+//   window.SpeechRecognition || window.webkitSpeechRecognition;
+// const start = document.querySelector("#start");
+// const end = document.querySelector("#end");
+// const list = document.querySelector("#list");
+// const recognition = new window.SpeechRecognition();
+// recognition.interimResults = true;
+// recognition.lang = "en-US";
 
-// console.log(arr2.push("Eng oxirgi qoshiluchi elements"));
+// let stop = false;
+// recognition.addEventListener("result", (e) => {
+//   if (e.results[0].isFinal) {
+//     const arr = Array.from(e.results[0])
+//       .map((item) => item.transcript)
+//       .join("");
 
-// console.log(arr);
+//     const li = document.createElement("li");
+//     li.textContent = arr;
 
-// Elementni oxiridan ochirib beradi
-// console.log(arr2.pop());
-// console.log(arr);
-// Elementni boshidan ochiriib berarakan
-// console.log(arr2.shift());
+//     list.appendChild(li);
+//   }
+// });
 
-// console.log(arr2);
+// recognition.addEventListener("end", (e) => {
+//   if (!stop) {
+//     recognition.start();
+//   }
+// });
 
-// Elementni boshidan qoshib berarkan
+// start.addEventListener("click", () => {
+//   recognition.start();
+// });
+// end.addEventListener("click", () => {
+//   recognition.stop();
 
-// console.log(arr2.unshift("Salom man birinchilardan qoshildim"));
+//   stop = true;
+// });
 
-// console.log(arr2);
+// function zipZap(str) {
+//   let result = "";
 
-// Elementni har yerdan qoshish uchun splice degan method kerak
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === "z" && str[i + 2] === "p") {
+//       result += str[i] + str[i + 2];
+//       i += 2;
+//     } else {
+//       result += str[i];
+//     }
+//   }
 
-// console.log(arr2.splice(0, 1, 3, 5, 6));
-
-// console.log(arr);
-
-// console.log(arr2.slice(0, 2));
-
-// console.log(arr);
-
-// let result = [];
-// for (let i = 0; i < name.length; i++) {
-//   result.push(name[i]);
+//   console.log(result);
+//   return result;
 // }
-// let name = "Saluomuuuqalesila";
 
-// console.log(name.split("u"));
+// zipZap("zipXzap");
 
-// console.log(arr.join("-"));
-// let qiymat = "gender";
+function salom(memoryValue) {
+  let counter = memoryValue;
 
-// let person = {
-//   name: "Shavkat",
-//   age: 20,
-//   gender: "male",
+  return function (num) {
+    if (counter === num) {
+      return "Siz Javobni topdingiz";
+    }
 
-//   subjects: {
-//     sports: ["rUNING"],
-//   },
-// };
-
-// console.log(person[qiymat]);
-
-// person[qiymat] = "Salom";
-
-// console.log(person);
-
-// delete person.age;
-
-// console.log(person);
-
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const list = document.getElementById("list");
-const modalaayer = document.querySelector(".modalaayer");
-const editedInput = document.querySelector("#editedInput");
-const editForm = document.querySelector("#editForm");
-
-let todoApptext = [
-  {
-    text: "Salom",
-    id: 99999999,
-  },
-];
-let index = 0;
-
-let editItem;
-
-renderHtmlElements();
-function createHtmlElemenets(e) {
-  e.preventDefault();
-
-  todoApptext.push({
-    text: input.value,
-    id: index,
-  });
-
-  index++;
-
-  renderHtmlElements();
-
-  // const li = document.createElement("li");
-  // li.setAttribute("class", "item");
-  // let listContent = document.createElement("div");
-  // todoApptext.forEach((item) => {
-  //   const li = document.createElement("li");
-  //   li.setAttribute("class", "item");
-
-  //   const img = document.createElement("img");
-  //   img.setAttribute("src", "./img/green_krug.svg");
-  //   const p = document.createElement("p");
-  //   p.textContent = item;
-  //   li.appendChild(p);
-  //   li.appendChild(img);
-  //   console.log(li);
-  //   // result += li;
-  //   listContent.appendChild(li);
-  // });
-  // console.log(listContent);
-  // list.replaceChildren(listContent);
-}
-
-function renderHtmlElements() {
-  let result = "";
-
-  todoApptext.forEach((item, index) => {
-    result =
-      result +
-      `<li class="item"> ${item.text}
-
-    <img class="krug"  src="./img/green_krug.svg" onclick="showModal(${item.id})" alt="icon">
-    <img class=" icon" src="./img/delete.svg" onclick="removeTodoApp(${item.id})" alt="icon"> </li>`;
-  });
-
-  list.innerHTML = result;
-}
-form.addEventListener("submit", createHtmlElemenets);
-
-function removeTodoApp(id) {
-  todoApptext = todoApptext.filter((item) => item.id !== id);
-
-  renderHtmlElements();
-}
-
-function showModal(id) {
-  modalaayer.style.display = "flex";
-
-  let findEndObject = todoApptext.find((item) => {
-    return item.id === id;
-  });
-  let findEndObjectIndex = todoApptext.findIndex((item) => {
-    return item.id === id;
-  });
-  editedInput.value = findEndObject.text;
-
-  editItem = {
-    index: findEndObjectIndex,
-    id: id,
+    if (num > counter) {
+      return "Siz kichkinaroq raqam ayting ayting";
+    }
+    if (num < counter) {
+      return "Siz katarroq raqam ayting ayting";
+    }
   };
 }
 
-function editFormHandler(e) {
-  e.preventDefault();
+// let makeCounter = salom(10);
 
-  todoApptext.splice(editItem.index, 1, {
-    text: editedInput.value,
-    id: editItem.id,
-  });
-
-  modalaayer.style.display = "none";
-  renderHtmlElements();
-}
-
-editForm.addEventListener("submit", editFormHandler);
-modalaayer.addEventListener("click", (e) => {
-  console.log(e.target);
-  if (e.target == modalaayer) modalaayer.style.display = "none";
-});
-
-const arr = ["Yanvar", "Fevral", "Mart"];
-// console.log(new Date().getFullYear());
-// console.log(new Date().getDay());
-// console.log(arr[new Date().getMonth()]);
-// console.log(
-//   `${new Date("2030-01-31T19:00:00").getFullYear()}.${new Date(
-//     "2030-01-31T19:00:00"
-//   ).getMonth()}`
-// );
-
-// let d = new Date("2030-01-31T19:10:00");
-
-// let datestring =
-//   d.getDate() +
-//   "." +
-//   (d.getMonth() + 1) +
-//   "." +
-//   d.getFullYear() +
-//   " " +
-//   d.getHours() +
-//   ":";
-// console.log(d.getMinutes());
-// if (
-//   d.getMinutes().toString().length === 1 &&
-//   d.getMinutes().toString() === "0"
-// ) {
-//   datestring += "00";
-// } else {
-//   datestring += d.getMinutes();
-// }
-// // 16.5.2015 9:50
-
-// console.log(datestring);
-
-// console.log("00004555555".padStart(15, "#"));
-
-// const person = {
-//   name: "Shavkat",
-//   age: 20,
-//   gender: "Male",
-// };
-
-// for (let key in person) {
-//   console.log(person[key]);
+// let num = 0;
+// while (makeCounter(num) !== "Siz Javobni topdingiz") {
+//   num = +prompt("Son ayting");
+//   console.log(makeCounter(num));
 // }
 
-// console.log();
-
-// Object.keys(person).forEach((item) => {
-//   console.log(person[item]);
-// });
-
-// console.log(Object.values(person));
+// console.log(this.salom);
