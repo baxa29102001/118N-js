f130299c56d3cd0b17a53406fa8563144139a423
mr_baxa <bohodir29102001@gmail.com>
2023-03-17 18:49:59 +0500

    fix

--- main.js
+++ main.js
@@ -4,44 +4,33 @@ const carousel_container = document.querySelector(".carousel_container");
 const carousel_containerImages = document.querySelectorAll(
   ".carousel_container img"
 );
-let index = 0;
+let index = 1;
+carousel_container.style.transform = `translateX(${-index * 400}px)`;
 btn.addEventListener("click", () => {
+  if (index >= carousel_containerImages.length - 1) return;
   index++;
   carousel_container.style.transition = "transform 0.5s ease-in-out";
-  carousel_container.style.transform = `translateX(-${index * 400}px)`;
+  carousel_container.style.transform = `translateX(${-index * 400}px)`;
+});
+before.addEventListener("click", () => {
+  if (index <= 0) return;
+  carousel_container.style.transition = "transform 0.5s ease-in-out";
+  index--;
+  carousel_container.style.transform = `translateX(${-index * 400}px)`;
 });
 
 carousel_container.addEventListener("transitionend", (e) => {
   if (carousel_containerImages[index].id === "lastElement") {
-    index = 0;
     carousel_container.style.transition = "none";
-    carousel_container.style.transform = `translateX(-${index * 400}px)`;
+    index = 1;
+    carousel_container.style.transform = `translateX(${-index * 400}px)`;
   }
-});
 
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new window.SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = "en-US";
-
-btn.addEventListener("click", () => {
-  recognition.start();
-});
-
-recognition.addEventListener("result", (e) => {
-  console.log(
-    Array.from(e.results)
-      .map((item) => item[0])
-      .map((item) => item.transcript)
-      .join(" ")
-  );
-
-  console.log(e.results[0]);
-});
-
-recognition.addEventListener("end", () => {
-  recognition.start();
+  if (carousel_containerImages[index].id === "firstElement") {
+    carousel_container.style.transition = "none";
+    index = carousel_containerImages.length - 2;
+    carousel_container.style.transform = `translateX(${-index * 400}px)`;
+  }
 });
 
 // function repeatSeparator(word, seperator, count) {
@@ -90,19 +79,19 @@ recognition.addEventListener("end", () => {
 
 // getSandwich("xxbreadyy");
 
-function zipZap(str) {
-  let result = "";
+// function zipZap(str) {
+//   let result = "";
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "z" && str[i + 2] === "p") {
-      result += str[i];
-      i += 2;
-    } else {
-      result += str[i];
-    }
-  }
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === "z" && str[i + 2] === "p") {
+//       result += str[i];
+//       i += 2;
+//     } else {
+//       result += str[i];
+//     }
+//   }
 
-  console.log(result);
-}
+//   console.log(result);
+// }
 
-zipZap("zipXzap");
+// zipZap("zipXzap");
